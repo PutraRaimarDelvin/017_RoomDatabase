c4a950be30449f10d0b726fbea1159da4d267b7c
PutraRaimarDelvin <delvinputra805@gmail.com>
2025-12-02 16:44:27 +0700

    tambahkan konfigurasi DatabaseSiswa

---
+++ app/src/main/java/com/example/pertemuan9/room/DatabaseSiswa.kt
@@ -0,0 +1,26 @@
+package com.example.pertemuan9.room
+
+import android.content.Context
+import androidx.room.Database
+import androidx.room.Room
+import androidx.room.RoomDatabase
+
+@Database(entities = [Siswa::class], version = 1, exportSchema = false)
+abstract class DatabaseSiswa : RoomDatabase() {
+    abstract fun siswaDao() : SiswaDao
+
+    companion object {
+        @Volatile
+        private var Instance: DatabaseSiswa? = null
+
+        fun getDataBase(context: Context): DatabaseSiswa {
+            return (Instance?: synchronized(this){
+                Room.databaseBuilder(
+                    context, DatabaseSiswa::class.java,
+                    "Siswa_Database")
+                    .build().also { Instance=it }
+            })
+        }
+    }
+}
+
