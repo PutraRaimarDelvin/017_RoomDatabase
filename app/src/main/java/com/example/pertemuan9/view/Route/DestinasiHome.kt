4d854e4d5d106c7a29385a12819cb8e8af4bba3a
PutraRaimarDelvin <delvinputra805@gmail.com>
2025-12-02 16:47:15 +0700

    membuat DestinasiHome untuk mendefinisikan rute 'home' dan judul navigasi aplikasi

---
+++ app/src/main/java/com/example/pertemuan9/view/Route/DestinasiHome.kt
@@ -0,0 +1,9 @@
+package com.example.pertemuan9.view.Route
+
+import com.example.pertemuan9.R
+
+
+object DestinasiHome : DestinasiNavigasi {
+    override val route = "home"
+    override val titleRes = R.string.app_name
+}
