28ece3cc47e324d004bb40176e55795b29bfdd03
PutraRaimarDelvin <delvinputra805@gmail.com>
2025-12-02 16:45:35 +0700

    tambah DestinasiEntry untuk navigasi

---
+++ app/src/main/java/com/example/pertemuan9/view/Route/DestinasiEntry.kt
@@ -0,0 +1,9 @@
+package com.example.pertemuan9.view.Route
+
+
+import com.example.pertemuan9.R
+
+object DestinasiEntry : DestinasiNavigasi {
+    override val route = "item_entry"
+    override val titleRes = R.string.entry_siswa
+}
