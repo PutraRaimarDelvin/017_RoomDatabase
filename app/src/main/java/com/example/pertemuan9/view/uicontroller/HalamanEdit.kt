00082b37c7c7c51050eb2918a0c1d96845018ab8
PutraRaimarDelvin <delvinputra805@gmail.com>
2025-12-05 12:38:26 +0700

    menambahkan EditSiswaScreen untuk memperbarui sisw yang ada

---
+++ app/src/main/java/com/example/pertemuan9/view/uicontroller/HalamanEdit.kt
@@ -0,0 +1,54 @@
+package com.example.pertemuan9.view.uicontroller
+
+
+import androidx.compose.foundation.layout.padding
+import androidx.compose.material3.ExperimentalMaterial3Api
+import androidx.compose.material3.Scaffold
+import androidx.compose.runtime.Composable
+import androidx.compose.runtime.rememberCoroutineScope
+import androidx.compose.ui.Modifier
+import androidx.compose.ui.res.stringResource
+import androidx.lifecycle.ViewModelProvider
+import androidx.lifecycle.viewmodel.compose.viewModel
+import com.example.pertemuan9.view.EntrySiswaBody
+import com.example.pertemuan9.view.Route.DestinasiEditSiswa
+import com.example.pertemuan9.view.SiswaTopAppBar
+import com.example.pertemuan9.viewmodel.EditViewModel
+import com.example.pertemuan9.viewmodel.provider.PenyediaViewModel
+import kotlinx.coroutines.launch
+
+
+
+
+@OptIn(ExperimentalMaterial3Api::class)
+@Composable
+fun EditSiswaScreen (
+    navigateBack: () -> Unit,
+    onNavigateUp: () -> Unit,
+    modifier: Modifier = Modifier,
+    viewModel: EditViewModel = viewModel(factory = PenyediaViewModel.factory)
+) {
+    Scaffold(
+        topBar = {
+            SiswaTopAppBar (
+                title = stringResource(DestinasiEditSiswa.titleRes),
+                canNavigateBack = true,
+                navigateUp = onNavigateUp
+            )
+        },
+        modifier = modifier
+    ) { innerPadding ->
+        val coroutineScope = rememberCoroutineScope()
+        EntrySiswaBody(
+            uiStateSiswa = viewModel.uiStateSiswa,
+            onValueChange = viewModel::updateUiState,
+            onSaveClick = {
+                coroutineScope.launch {
+                    viewModel.updateSiswa()
+                    navigateBack()
+                }
+            },
+            modifier = Modifier.padding(innerPadding)
+        )
+    }
+}
