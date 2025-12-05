0a8fa5837b2b76fd504e43fc87ef37ffee325abe
PutraRaimarDelvin <delvinputra805@gmail.com>
2025-12-05 12:44:32 +0700

    menambahkan penyedia ViewModel untuk mengatur inisialisasi HomeViewModel, EntryViewModel, DetailViewModel, dan EditViewModel melalui ViewModelFactory

--- app/src/main/java/com/example/pertemuan9/viewmodel/provider/PenyediaViewModel.kt
+++ app/src/main/java/com/example/pertemuan9/viewmodel/provider/PenyediaViewModel.kt
@@ -1,15 +1,19 @@
 package com.example.pertemuan9.viewmodel.provider
 
 import androidx.lifecycle.ViewModelProvider
+import androidx.lifecycle.createSavedStateHandle
 import androidx.lifecycle.viewmodel.CreationExtras
 import androidx.lifecycle.viewmodel.initializer
 import androidx.lifecycle.viewmodel.viewModelFactory
-import com.example.pertemuan9.repositori.AplikasiSiswa
-import com.example.pertemuan9.viewmodel.EntryViewModel
 import com.example.pertemuan9.viewmodel.HomeViewModel
+import com.example.pertemuan9.repositori.AplikasiSiswa
+import com.example.pertemuan9.viewmodel.DetailViewModel
+import com.example.pertemuan9.viewmodel.EditViewModel
+import com.example.pertemuan9.viewmodel.EntryViewModel
+
 
 object PenyediaViewModel {
-    val Factory = viewModelFactory {
+    val factory = viewModelFactory {
         initializer {
             HomeViewModel(aplikasiSiswa().container.repositoriSiswa)
         }
@@ -17,8 +21,19 @@ object PenyediaViewModel {
         initializer {
             EntryViewModel(aplikasiSiswa().container.repositoriSiswa)
         }
+
+        initializer {
+            DetailViewModel(this.createSavedStateHandle(), aplikasiSiswa().container.repositoriSiswa)
+        }
+
+        initializer {
+            EditViewModel(this.createSavedStateHandle(), aplikasiSiswa().container.repositoriSiswa)
+        }
     }
 }
-
+/**
+ * Fungsi ekstensi query untuk objek [Application] dan mengembalikan sebuah instance dari
+ * [AplikasiSiswa].
+ */
 fun CreationExtras.aplikasiSiswa(): AplikasiSiswa =
     (this[ViewModelProvider.AndroidViewModelFactory.APPLICATION_KEY] as AplikasiSiswa)
